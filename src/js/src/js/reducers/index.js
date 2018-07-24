0dab9e516d118c32fd66ed36c8b1d8589bc64824
sandond77 <sandu@ucdavis.edu>
2018-07-24 15:10:30 -0700

    removed false comment from reducer file

--- src/js/src/js/reducers/index.js
+++ src/js/src/js/reducers/index.js
@@ -1,8 +1,9 @@
-// src/js/reducers/index.js
 const initialState = {
   articles: []
 };
 
 const rootReducer = (state = initialState, action) => state;
+//rootReducer here is the reducer; it is taking 2 parameter - state and action
+//we are setting state equal to the constant initialState
 
 export default rootReducer;
