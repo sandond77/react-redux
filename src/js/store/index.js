d1048429be275dfae4b6daa09e667f8a2de71672
sandond77 <sandu@ucdavis.edu>
2018-07-24 15:09:47 -0700

    started reducer tutorial

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers/index";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer); //createStore is a function for creating the redux store
 
 export default store;
