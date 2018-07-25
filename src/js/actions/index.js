5c0c34111b77baee3e005fa65427757f243007df
sandond77 <sandu@ucdavis.edu>
2018-07-24 23:03:30 -0700

    created action-types.js file

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,3 +1,5 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
 export const addArticle = article => ({
 	type: "ADD_ARTICLE",
 	payload: article
---
+++ src/js/constants/action-types.js
@@ -0,0 +1,3 @@
+// src/js/coinstants/action-types.js
+
+export const ADD_ARTICLE = "ADD_ARTICLE";
