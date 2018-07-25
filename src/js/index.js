84db681ad5a282d9da7dd696b44eedb7d0e17367
sandond77 <sandu@ucdavis.edu>
2018-07-24 23:49:09 -0700

    added lines for redux state change tutorial

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,11 @@ import { addArticle } from "../js/actions/index";
 
 window.store = store;
 window.addArticle = addArticle;
+
+// store.subscribe(() => console.log('Look ma, redux!!'));
+
+// store.dispatch( addArticle ({
+// 	name: 'React Redux Tutorial for Noobs',
+// 	id: 1
+// 	})
+// )
