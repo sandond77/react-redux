5944e82d261d3bb95f6dc1cfda64d3a2e1bcba42
sandond77 <sandu@ucdavis.edu>
2018-07-24 23:11:04 -0700

    updated reducer index.js file to include add_article case

--- src/js/src/js/reducers/index.js
+++ src/js/src/js/reducers/index.js
@@ -2,7 +2,17 @@ const initialState = {
   articles: []
 };
 
-const rootReducer = (state = initialState, action) => state;
+
+
+const rootReducer = (state = initialState, action) => {
+	switch (action.type){
+		case ADD_ARTICLE:
+			state.articles.push(action.payload);
+			return state;
+		default:
+			return state;
+	}
+}
 //rootReducer here is the reducer; it is taking 2 parameter - state and action
 //we are setting state equal to the constant initialState
 
