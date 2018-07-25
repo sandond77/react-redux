f784b06e0a03d7c9d381930463f1e8d096c9f89c
sandond77 <sandu@ucdavis.edu>
2018-07-24 23:29:54 -0700

    added object spread plug in and made reducer immutable

--- src/js/src/js/reducers/index.js
+++ src/js/src/js/reducers/index.js
@@ -7,8 +7,12 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
 	switch (action.type){
 		case ADD_ARTICLE:
-			state.articles.push(action.payload);
-			return state;
+			// state.articles.push(action.payload); 
+			//this is a bad reducer because it our state is not immutable; we are adding a state object
+			return { ...state, articles: states.articles.concat(action.payload)};
+			// we are using ... (the es6 spread function) and concat() to create a new array; 
+			// the spread function ... will save us some time by taking all elements in the state array;
+			// concat inheritly creates a new array
 		default:
 			return state;
 	}
