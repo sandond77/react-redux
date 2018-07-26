9939cc9f20aebe7e4ffb6bd39d4b7d5814e4426f
sandond77 <sandu@ucdavis.edu>
2018-07-25 23:39:53 -0700

    fixed List.js file. app is now properly rendering

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store/index";
 import App from "./components/App";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 render(
 		<Provider store={store}>
